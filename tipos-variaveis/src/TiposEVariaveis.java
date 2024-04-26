850b9dc1f06b52482d3b94fda0b37094d309f288
Matheus Vinicius de Alexandro Martins <145573301+Mvdam1@users.noreply.github.com>
2024-04-26 18:33:57 -0300

    Add files via upload
    Arquivos da primeira aula, com anotações.

---
+++ tipos-variaveis/src/TiposEVariaveis.java
@@ -0,0 +1,25 @@
+public class TiposEVariaveis {
+    public static void main(String[] args) throws Exception {
+        // double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+    }
+}
+// Java é fortemente tipado
+// centavos se usa .
+// if (salarioMinimo < 5222)
+// System.out.println("Ta pobre em porra");
+// else if (salarioMinimo == 2500)
+// System.out.println("Só isso?");
+// else
+// System.out.println("Foda");
+
+// byte idade = 123;
+// short ano = 2021;
+// int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+// long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro.
+// Finaliza com L para mostar que sera tipo longo
+// tipo
+// float pi = 3.14F;
+// double salario = 1275.33;
